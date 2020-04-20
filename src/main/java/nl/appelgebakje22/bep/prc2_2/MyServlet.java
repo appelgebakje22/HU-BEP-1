68b3650884377b55b7dd717b9941a328e577cc75
appelgebakje22 <appelgebakje22@xeromeda.com>
2020-04-20 12:39:01 +0200

    Added parallel-test

---
+++ src/main/java/nl/appelgebakje22/bep/prc2_2/MyServlet.java
@@ -0,0 +1,63 @@
+package nl.appelgebakje22.bep.prc2_2;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+final class MyServlet extends Thread {
+
+	private final Socket socket;
+
+	MyServlet(Socket sock) {
+		super("Client-Thread: " + sock.getRemoteSocketAddress());
+		this.socket = sock;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// Request
+			Scanner scanner = new Scanner(this.socket.getInputStream());
+			String path = null;
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if (path == null) // Always true for the first line in the request
+					path = line.split(" ")[1];
+				if (line.isEmpty())
+					break;
+			}
+			System.out.println("Requested path: " + path);
+
+			// Response
+			File requestedFile = new File("." + path).getAbsoluteFile();
+
+			OutputStream output = this.socket.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output));
+			if (!requestedFile.exists() || requestedFile.isDirectory()) {
+				writer.write("HTTP/1.1 404 File Not Found\r\n");
+				writer.write("\r\n");
+				writer.write("<h1>404: File not found!</h1>\r\n");
+			} else {
+				writer.write("HTTP/1.1 200 OK\r\n");
+				writer.write("\r\n");
+				writer.flush();
+				output.write(Files.readAllBytes(requestedFile.toPath()));
+				output.flush();
+				writer.write("\r\n");
+			}
+			writer.flush();
+			this.socket.close();
+		} catch (IOException e) {
+			System.err.println("Exception in thread: " + this.getName());
+			e.printStackTrace();
+		}
+		try {
+			System.out.println("Thread " + this.getName() + " will now sleep for 10 seconds!");
+			Thread.sleep(10 * 1000L);
+			System.out.println("Thread " + this.getName() + " is done sleeping!");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/nl/appelgebakje22/bep/prc2_2/Server.java
@@ -0,0 +1,54 @@
+package nl.appelgebakje22.bep.prc2_2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Scanner;
+import java.util.function.Supplier;
+
+public final class Server {
+
+	public static void main(String[] args) {
+		//Create server
+		Supplier<ServerSocket> serverSupplier = () -> {
+			try {
+				return new ServerSocket(3050);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		};
+		final ServerSocket server = serverSupplier.get();
+		if (server == null) {
+			Runtime.getRuntime().halt(1);
+		}
+
+		//Create stdin listener thread
+		new Thread(() -> {
+			while (true) {
+				Scanner scanner = new Scanner(System.in);
+				String command = scanner.nextLine();
+				if (command.equalsIgnoreCase("quit")) {
+					System.err.println("Quit command received!");
+					try {
+						server.close();
+					} catch (IOException ignored) {
+						//NOOP
+					}
+					break;
+				}
+			}
+		}, "Input listener").start();
+
+		//Open server
+		while (true) {
+			try {
+				new MyServlet(server.accept()).start();
+			} catch (IOException e) {
+				//We don't care about errors caused by closing the server in the "Input Listener" thread
+				if (e.getMessage().equalsIgnoreCase("Socket closed"))
+					break;
+				e.printStackTrace();
+			}
+		}
+	}
+}
